a3081415af7f20481fdb40ee64e3322a48673ee0
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-10-04 23:53:54 +1000

    http basics,headers, request object

---
+++ 01-node-tutorial/17-http-stream.js
@@ -0,0 +1,17 @@
+import http from 'http';
+import fs from 'fs';
+
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8');
+    // res.end(text);
+
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStream.on('open', () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on('error', (err) => {
+      res.end(err);
+    });
+  })
+  .listen(8000);
